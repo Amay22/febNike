ad42f20fb7cba383472b61319b6a382e168f3636
amay22 <amay@nyu.edu>
2016-03-13 21:48:19 -0400

    Added Title DAO

---
+++ src/main/nike/dao/TitleDAO.java
@@ -0,0 +1,30 @@
+package nike.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import nike.model.Title;
+
+@Repository
+public interface TitleDAO {
+	
+	public Title addTitle(Title t);
+
+	public Title updateTitle(Title t);
+
+	public Title removeTitle(int id);
+	
+	public List<Title> listTitles();
+	
+	public Title getTitleById(int id);
+	
+	public List<Title> getTitleBySearchTerm(String title);
+	
+	public List<Title> getTitleByYear(int year);
+	
+	public List<Title> getTitleByType(String type);
+	
+	public List<Title> getTitleByGenre(String genre);
+	
+}
